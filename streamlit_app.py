724195775ac1f66436546ac97cc78a43330dfa8f
Atharva7775 <65668471+Atharva7775@users.noreply.github.com>
2024-04-02 11:18:42 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ import os
 from transformers import CLIPProcessor, CLIPModel
 CLIPProcessor.safety_checker = None
 
-@st.cache(allow_output_mutation=True)
+st.cache_resource
 def img2text(uploaded_file):
     model = CLIPModel.from_pretrained("openai/clip-vit-large-patch14-336")
     processor = CLIPProcessor.from_pretrained("openai/clip-vit-large-patch14-336")
